baf270697ca4aacc95a42bec09c160547e2699e0
Shashank Upadhyay <shashank_@outlook.in>
2024-05-08 15:23:32 +0530

    .

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 app.use(rateLimit({
-    windowMs: 24 * 60 * 60 * 1000,
+    windowMs: 1 * 60 * 60 * 1000,
     max: 100,
     message: 'You have exceeded the 100 requests in 24 hrs limit!',
     standardHeaders: true,
